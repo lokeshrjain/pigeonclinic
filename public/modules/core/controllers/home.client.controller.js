1f2caacf718ae5b687c37275095097bee02a01ac
lokesh <lokeshrjain@gmail.com>
2014-12-07 00:25:04 +0000

    changed double quote to single quote for home page json

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -7,41 +7,41 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 		$scope.authentication = Authentication;
 		$scope.alerts = [
 			{
-				icon: "glyphicon-user",
-				color: "btn-success",
-				total: "20,408",
-				description:"TOTAL CUSTOMERS"
+				icon: 'glyphicon-user',
+				color: 'btn-success',
+				total: '20,408',
+				description:'TOTAL CUSTOMERS'
 			},
 			{
-				icon: "glyphicon-calendar",
-				color: "btn-primary",
-				total: "8,382",
-				description:"UPCOMING EVENTS"
+				icon: 'glyphicon-calendar',
+				color: 'btn-primary',
+				total: '8,382',
+				description:'UPCOMING EVENTS'
 			},
 
 			{
-				icon: "glyphicon-edit",
-				color: "btn-success",
-				total: "527",
-				description:"NEW CUSTOMERS IN 24H"
+				icon: 'glyphicon-edit',
+				color: 'btn-success',
+				total: '527',
+				description:'NEW CUSTOMERS IN 24H'
 			},
 			{
-				icon: "glyphicon-record",
-				color: "btn-info",
-				total: "85,000",
-				description:"EMAILS SENT"
+				icon: 'glyphicon-record',
+				color: 'btn-info',
+				total: '85,000',
+				description:'EMAILS SENT'
 			},
 			{
-				icon: "glyphicon-eye-open",
-				color: "btn-warning",
-				total: "268",
-				description:"FOLLOW UP REQUIRED"
+				icon: 'glyphicon-eye-open',
+				color: 'btn-warning',
+				total: '268',
+				description:'FOLLOW UP REQUIRED'
 			},
 			{
-				icon: "glyphicon-flag",
-				color: "btn-danger",
-				total: "348",
-				description:"REFERRALS TO MODERATE"
+				icon: 'glyphicon-flag',
+				color: 'btn-danger',
+				total: '348',
+				description:'REFERRALS TO MODERATE'
 			}
 		]
 	}
